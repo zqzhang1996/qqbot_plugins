3ff17bc7b7145915dd65ef414a206d1ddd58ded1
zqzhang1996 <Pr0phet.chn@outlook.com>
2018-11-09 19:03:15 +0800

    现在不会连续复读一样的话了

--- repeater.py
+++ repeater.py
@@ -1,6 +1,7 @@
 # -- coding utf-8 --
 
 messageDict = {}
+RepeatedMessageDict ={}
 
 def onQQMessage(bot, contact, member, content):
 	if(not bot.isMe(contact, member)):
@@ -11,7 +12,10 @@ def onQQMessage(bot, contact, member, content):
 			messageDict[uid].append(content)
 		if(len(messageDict[uid]) > 10):
 			messageDict[uid].pop(0)
-		if(messageDict[uid].count(content) > 1):
+		if(not RepeatedMessageDict.__contains__(uid)):
+			RepeatedMessageDict[uid]=['',]
+		if(messageDict[uid].count(content) > 1) and (RepeatedMessageDict[uid].count(content) == 0):
 			bot.SendTo(contact, content)
-			while(messageDict[uid].count(content) > 0):
-				messageDict[uid].remove(content)
+			RepeatedMessageDict[uid].append(content)
+			if(len(RepeatedMessageDict[uid]) > 3):
+				RepeatedMessageDict[uid].pop(0)
