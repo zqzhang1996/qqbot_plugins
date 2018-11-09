0c49eed3a473663250e296a9fe5d3cf4a4c45784
zqzhang1996 <Pr0phet.chn@outlook.com>
2018-11-09 11:40:08 +0800

    Update sample.py

--- sample.py
+++ sample.py
@@ -8,3 +8,11 @@ def onQQMessage(bot, contact, member, content):
 		bot.SendTo(contact, '放我下来！')
 	elif (not bot.isMe(contact, member))and('嘤' in content):
 		bot.SendTo(contact, ('@' + (member.name if (contact.ctype != 'buddy') else contact.name) + ' ')+'一拳一个嘤嘤怪')
+	elif content == '插件开发':
+		bot.SendTo(contact, 'GitHub地址：https://github.com/zqzhang1996/qqbot_plugins')
+	elif content == 'GitHub教程':
+		bot.SendTo(contact, 'http://wiki.jikexueyuan.com/project/github-basics/')
+	elif content == 'Python教程':
+		bot.SendTo(contact, 'http://www.runoob.com/python/python-tutorial.html')
+	elif content == '教练我想学':
+		bot.SendTo(contact, '你已经是个成熟的大学生了，应该学会自己使用搜索引擎')
